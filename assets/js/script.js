b0036503e6c32c7ca1fc2b8b2283ef4b1cce4cdf
Taweria <elodieali.pro@gmail.com>
2024-01-03 14:20:42 +0100

    Radar graph and line graph

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var options = {
+var optionsChart1 = {
   chart: {
     animations: {
         enabled: true,
@@ -16,6 +16,50 @@ var options = {
   }
 }
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
+var chart1 = new ApexCharts(document.querySelector("#chart1"), optionsChart1);
 
-chart.render();
+chart1.render();
+
+var optionsChart2 = {
+  chart: {
+    animations: {
+        enabled: true,
+        easing: 'easeinout',
+        speed: 800,
+},
+    type: 'radar'
+  },
+  series: [{
+    name: 'sales',
+    data: [30,40,45,50,49,60,70,91,125]
+  }],
+  xaxis: {
+    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+  }
+}
+
+var chart2 = new ApexCharts(document.querySelector("#chart2"), optionsChart2);
+
+chart2.render();
+
+var optionsChart3 = {
+  chart: {
+    animations: {
+        enabled: true,
+        easing: 'easeinout',
+        speed: 800,
+},
+    type: 'line'
+  },
+  series: [{
+    name: 'sales',
+    data: [30,40,45,50,49,60,70,91,125]
+  }],
+  xaxis: {
+    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+  }
+}
+
+var chart3 = new ApexCharts(document.querySelector("#chart2"), optionsChart3);
+
+chart3.render();
